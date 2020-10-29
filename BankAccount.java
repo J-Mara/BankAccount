5e56261e3cf11dbdd0e6879bd027df42da0c4f8b
J-Mara <jmaragakis20@stuy.edu>
2020-10-28 22:36:21 -0400

    First commit, hapefully no more git problems

---
+++ BankAccount.java
@@ -0,0 +1,45 @@
+public class BankAccount{
+
+  private double balance;
+  private int accountID;
+  private String password;
+
+  public BankAccount(int ID, String p){
+    balance = 0;
+    accountID = ID;
+    password = p;
+  }
+
+  public double getBalance(){
+    return balance;
+  }
+
+  public int getAccountID(){
+    return accountID;
+  }
+
+  public void setPassword(String p){
+    password = p;
+  }
+
+  public boolean deposit(double amount){
+    if(amount >= 0){
+      balance += amount;
+      return true;
+    }
+    return false;
+  }
+
+  public boolean withdraw(double amount){
+    if(amount < 0 || balance - amount < 0){
+      return false;
+    }
+    balance -= amount;
+    return true;
+  }
+
+  public String toString(){
+    return accountID + "\t" + balance;
+  }
+
+}
